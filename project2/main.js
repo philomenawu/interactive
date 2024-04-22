005c7b782b3b4c521c0d82f2945e7665c2797dda
Philomena <128011445+philomenawu333@users.noreply.github.com>
2024-04-21 21:44:52 -0400

    web41

--- project2/main.js
+++ project2/main.js
@@ -1,81 +1,147 @@
-// Pagecreate will fire for each of the pages in this demo
-// but we only need to bind once so we use "one()"
-$( document ).one( "pagecreate", ".demo-page", function() {
-    // Initialize the external persistent header and footer
-    $( "#header" ).toolbar({ theme: "b" });
-    $( "#footer" ).toolbar({ theme: "b" });
-    // Handler for navigating to the next page
-    function navnext( next ) {
-        $( ":mobile-pagecontainer" ).pagecontainer( "change", next + ".html", {
-            transition: "slide"
-        });
-    }
-    // Handler for navigating to the previous page
-    function navprev( prev ) {
-        $( ":mobile-pagecontainer" ).pagecontainer( "change", prev + ".html", {
-            transition: "slide",
-            reverse: true
-        });
-    }
-    // Navigate to the next page on swipeleft
-    $( document ).on( "swipeup", ".ui-page", function( event ) {
-        // Get the filename of the next page. We stored that in the data-next
-        // attribute in the original markup.
-        var next = $( this ).jqmData( "next" );
-        // Check if there is a next page and
-        // swipes may also happen when the user highlights text, so ignore those.
-        // We're only interested in swipes on the page.
-        if ( next && ( event.target === $( this )[ 0 ] ) ) {
-            navnext( next );
-        }
-    });
-    // Navigate to the next page when the "next" button in the footer is clicked
-    $( document ).on( "click", ".next", function() {
-        var next = $( ".ui-page-active" ).jqmData( "next" );
-        // Check if there is a next page
-        if ( next ) {
-            navnext( next );
-        }
-    });
-    // The same for the navigating to the previous page
-    $( document ).on( "swiperight", ".ui-page", function( event ) {
-        var prev = $( this ).jqmData( "prev" );
-        if ( prev && ( event.target === $( this )[ 0 ] ) ) {
-            navprev( prev );
-        }
-    });
-    $( document ).on( "click", ".prev", function() {
-        var prev = $( ".ui-page-active" ).jqmData( "prev" );
-        if ( prev ) {
-            navprev( prev );
-        }
-    });
-});
-$( document ).on( "pageshow", ".demo-page", function() {
-    var thePage = $( this ),
-        title = thePage.jqmData( "title" ),
-        next = thePage.jqmData( "next" ),
-        prev = thePage.jqmData( "prev" );
-    // Point the "Trivia" button to the popup for the current page.
-    $( "#trivia-button" ).attr( "href", "#" + thePage.find( ".trivia" ).attr( "id" ) );
-    // We use the same header on each page
-    // so we have to update the title
-    $( "#header h1" ).text( title );
-    // Prefetch the next page
-    // We added data-dom-cache="true" to the page so it won't be deleted
-    // so there is no need to prefetch it
-    if ( next ) {
-        $( ":mobile-pagecontainer" ).pagecontainer( "load", next + ".html" );
-    }
-    // We disable the next or previous buttons in the footer
-    // if there is no next or previous page
-    // We use the same footer on each page
-    // so first we remove the disabled class if it is there
-    $( ".next.ui-state-disabled, .prev.ui-state-disabled" ).removeClass( "ui-state-disabled" );
-    if ( ! next ) {
-        $( ".next" ).addClass( "ui-state-disabled" );
-    }
-    if ( ! prev ) {
-        $( ".prev" ).addClass( "ui-state-disabled" );
-    }
-});
+var carousel = (function () {
+ 
+    //
+   var activeID = 0,
+       itemW = 940,
+       carousel_count = $('.carousel_item').length,
+       $carouselItems = $('.carousel_items'),
+       $carouselItem = $('.carousel_item'),
+       $arrowPrev = $('.item_prev'),
+       $arrowNext = $('.item_next'),
+   $itemArrow = $('.item_arrow'),
+       $navDot,
+   $navDots = $('.nav_dots'),
+       swipeDir,
+       slideSpeed = .45,
+       slideMeth = Power2.EaseInOut;
+   
+   //
+ function init() {
+    
+   $carouselItems.css({'width': (itemW * carousel_count) + 'px'});
+   $navDots.css({'width': (25 * carousel_count) + 'px'});
+   $itemArrow.css({'opacity': .8});
+   
+   setupDraggable();
+   setupDots();
+   navigateSlide();
+   }
+ init();
+   
+   //
+ function setupDraggable() { 
+     
+   Draggable.create($carouselItems, {
+           type:'x',
+           edgeResistance: 0.90,
+           dragResistance: 0.0,
+           bounds:'.carousel_container',
+           onDrag:updateDirections,
+           onThrowUpdate:updateDirections,
+           throwProps:true,
+           onDragStart:function(evt) {},
+           onDragEnd :function() {
+
+             if(swipeDir == 'left') {activeID++}
+             else if(swipeDir == 'right') {activeID--};
+             
+             navigateSlide();
+           }
+     });    
+ };
+               
+ // set up dots
+ function setupDots() {    
+   for(var i = 0; i < carousel_count; i++) {
+     $navDots.append('<div class="nav_dot" id="dot_' + i + '"></div>');
+   }    
+   $navDot = $('.nav_dot');
+ }  
+ 
+ // navigate slide
+   function navigateSlide() {
+       
+       if(activeID >= carousel_count-1) activeID = carousel_count-1;
+       if(activeID <= 0) activeID = 0;		
+                       
+       var xTarget = ((activeID * itemW) * -1);
+       
+       TweenMax.to($carouselItems, slideSpeed, {x: xTarget, ease: slideMeth, onComplete: slideDone});
+   }
+   
+   function slideDone() {
+       
+       $navDot.css({backgroundColor: '#fff'});
+   
+   //
+       TweenMax.to($navDot, .35, {scale: 1, color: 0xFFFFFF});
+       TweenMax.to($('#dot_' + activeID), .35, {scale: 1.5, backgroundColor: 'transparent',color: 0xCC0000});
+   
+   //
+   if(activeID == 0) {$arrowPrev.fadeOut()} 
+   else {$arrowPrev.fadeIn()}
+   
+   if(activeID + 1 == carousel_count) {$arrowNext.fadeOut()}
+   else {$arrowNext.fadeIn()}
+   }
+   
+   //
+   function updateDirections() {
+       swipeDir = this.getDirection("start");
+   }
+     
+ //$itemArrow.click(function() {
+ $itemArrow.on('click', function() {
+   
+   if(Modernizr.touch) return;
+   
+   if($(this).hasClass('item_next')) {activeID++}
+   else {activeID--};
+   
+   navigateSlide();
+   });
+ 
+ $itemArrow.on('touchstart', function() {
+   if($(this).hasClass('item_next')) {activeID++}
+   else {activeID--};
+   
+   navigateSlide();
+   });
+ 
+   $navDot.hover(		
+       function() {			
+           TweenMax.to($(this), .35, {scale: 1.5});
+       }, function() {
+            if($(this).attr('id').split('_')[1] == activeID) return;
+          TweenMax.to($(this), .35, {scale: 1.0});
+       }  
+   );
+   
+ $navDot.click(function() {		
+   var dotID = $(this).attr('id').split('_')[1];
+       activeID = dotID;
+           
+     navigateSlide();		
+   });
+ 
+   //
+   $carouselItem.mousedown(function() {		
+       activeID = $(this).attr('id').split('_')[1];
+   
+   $(this).removeClass('grab');
+   $(this).addClass('grabbing');
+   
+   });
+ 
+ //   
+ $carouselItem.mouseenter(function() {        
+   $(this).removeClass('grabbing');
+   $(this).addClass('grab');
+ });
+
+ $carouselItem.mouseup(function() {        
+   $(this).removeClass('grabbing');
+   $(this).addClass('grab');
+ });  
+ 
+})();
